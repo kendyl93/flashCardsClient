7309079c13f721e3407b753c857eecee9bb886cb
Pawel Stanecki <p.stanecki93@gmail.com>
2020-11-07 17:39:56 +0100

    Added basic configurations to the react-native-navigation

--- index.js
+++ index.js
@@ -2,8 +2,54 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { Navigation } from "react-native-navigation";
+import React  from 'react';
+import { View , Text, StyleSheet } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const HomeScreen = (props) => {
+    return (
+      <View style={styles.root}>
+        <Text>Hello React Native Navigation 👋</Text>
+      </View>
+    );
+  };
+
+  HomeScreen.options = {
+    topBar: {
+      title: {
+        text: 'Home',
+        color: 'white'
+      },
+      background: {
+        color: '#4d089a'
+      }
+    }
+}
+  
+
+  const styles = StyleSheet.create({
+    root: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'whitesmoke'
+    }
+  });
+
+Navigation.registerComponent('Home', () => HomeScreen);
+Navigation.events().registerAppLaunchedListener(() => {
+   Navigation.setRoot({
+     root: {
+       stack: {
+         children: [
+           {
+             component: {
+               name: 'Home'
+             }
+           }
+         ]
+       }
+     }
+  });
+});
